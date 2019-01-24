b1386b186a0bf83efc347f7a8def872a614c5358
Graham Smith <grahamas@gmail.com>
2019-01-24 16:06:54 -0600

    Removed old, unused field.

--- src/CalculatedTypes.jl
+++ src/CalculatedTypes.jl
@@ -6,14 +6,10 @@ function Calculated(a::T) where T
     error("Calculated type undefined for type $T")
 end
 
-function get_value(calculated_object::CalculatedType)
-    calculated_object.value
-end
-
 function update(olds::AACT}, news::AbstractArray{T}) where {T, CT<:CalculatedType{T},AACT<:AbstractArray{CT}}
     AACT[update(pair...) for pair in zip(olds, news)]
 end
 
-export CalculatedType, Calculated, update, get_value
+export CalculatedType, Calculated, update
 
 end
