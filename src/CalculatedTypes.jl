5c72df3e08cc99b0db604ebfda5b3d29da415c3a
Graham Smith <grahamas@gmail.com>
2019-01-24 15:57:59 -0600

    Fixed exports.

--- src/CalculatedTypes.jl
+++ src/CalculatedTypes.jl
@@ -1,7 +1,11 @@
 module CalculatedTypes
 
-
 abstract type CalculatedType{S} end
+
+function Calculated(a::T) where T
+    error("Calculated type undefined for type $T")
+end
+
 function get_value(calculated_object::CalculatedType)
     calculated_object.value
 end
@@ -10,12 +14,6 @@ function update(olds::AACT}, news::AbstractArray{T}) where {T, CT<:CalculatedTyp
     AACT[update(pair...) for pair in zip(olds, news)]
 end
 
-function Calculated(a::T) where T
-	error("Calculated type undefined for type $T")
-end
-
-export zero, default_value, bounds
-export update
-export get_value
+export CalculatedType, Calculated, update, get_value
 
 end
