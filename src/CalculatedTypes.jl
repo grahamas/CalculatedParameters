15e4a4872adb8507a092b59b81201edf4d9efd14
Graham Smith <grahamas@gmail.com>
2019-03-04 08:27:27 -0600

    Added macro to define calculated types

--- src/CalculatedTypes.jl
+++ src/CalculatedTypes.jl
@@ -1,5 +1,8 @@
 module CalculatedTypes
 
+using MacroTools, Espresso
+using Parameters
+
 abstract type CalculatedType{S} end
 
 function Calculated(a::T) where T
@@ -14,6 +17,36 @@ function update(olds::AACT, news::AbstractArray{T}) where {T, CT<:CalculatedType
     AACT[update(pair...) for pair in zip(olds, news)]
 end
 
-export CalculatedType, Calculated, update, get_value
+macro calculated_type(type_def_expr, calculation_body_expr, return_type=:Any)
+    @capture(type_def_expr,
+        struct (T_{PT__} | (T_{PT__} <: _))
+            fields__
+        end
+        )
+    field_names = [(@capture(field, name_::_) ? name : field) for field in fields]
+    field_mapping = Dict(name => :(source.$name) for name in field_names)
+    calculation_body_expr = subs(calculation_body_expr, field_mapping)
+    calculated_type_expr = :($(Symbol(:Calculated,T)){$(PT...)})
+    source_type_expr = :($T{$(PT...)})
+    return esc(quote
+        $(type_def_expr)
+        function $source_type_expr(; $(field_names...)) where {$(PT...)}
+             $source_type_expr($(field_names...))
+         end
+        function calculate(source::$(source_type_expr)) where {$(PT...)}
+            $(calculation_body_expr)
+        end
+        struct $(calculated_type_expr) <: CalculatedType{$(source_type_expr)}
+            source::$(source_type_expr)
+            value::$return_type
+            $calculated_type_expr(source::$(source_type_expr)) where {$(PT...)} = new(source, calculate(source))
+        end
+        function Calculated(source::$(source_type_expr)) where {$(PT...)}
+            $(calculated_type_expr)(source)
+        end
+    end)
+end
+
+export CalculatedType, Calculated, update, get_value, @calculated_type
 
 end
