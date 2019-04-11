d8c39f43b839dcd83aa502f80244963dc6f2972f
zeptodoctor <44736852+zeptodoctor@users.noreply.github.com>
2019-04-11 14:01:30 +0000

    build based on 2898d13

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#CalculatedTypes-Documentation-1","page":"CalculatedTypes Documentation","title":"CalculatedTypes Documentation","text":"","category":"section"},{"location":"#","page":"CalculatedTypes Documentation","title":"CalculatedTypes Documentation","text":"DocTestSetup = quote\n    using CalculatedTypes\nend","category":"page"},{"location":"#","page":"CalculatedTypes Documentation","title":"CalculatedTypes Documentation","text":"Precompute expensive objects and/or generic interface for calculating parametrized objects.","category":"page"}]
+}
